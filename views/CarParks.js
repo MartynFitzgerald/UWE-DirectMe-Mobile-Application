81d868b75acd074eded4934a9ea57b4ab4762150
Martyn Fitzgerald <Martyn2.Fitzgerald@live.uwe.ac.uk>
2020-04-02 14:29:23 +0100

    Added CarParks and Info views

---
+++ views/CarParks.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, ScrollView } from 'react-native';
+import { List, Title, Text, Button } from 'react-native-paper';
+
+export default class History extends Component {
+  render() {
+    return (
+      <View>
+        <Title style={styles.title}>Car Parks</Title>
+        <ScrollView>
+          <List.Section style={styles.list}>
+            <List.Item
+              title="Trenchard Street Car Park"
+              description="BS34 9AR"
+              left={() => <List.Icon color="#4285F4" icon="parking" />}
+              right={() => <List.Icon color="#4285F4" icon="chevron-right" />}
+              //onPress={(e) => navigation.navigate('Register')}
+            />
+              <List.Item
+                title="NCP Car Park Bristol Broadmead"
+                description="BS34 9AR"
+                left={() => <List.Icon color="#4285F4" icon="parking" />}
+                right={() => <List.Icon color="#4285F4" icon="chevron-right" />}
+                //onPress={(e) => navigation.navigate('Register')}
+            />
+        </List.Section>
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  list: {
+      width: '100%',
+      height: '100%',
+  },
+  title: {
+      paddingTop: 40,
+      marginTop: 0,
+      marginBottom: 0,
+      top: 0,
+      textAlign: 'center', 
+      fontWeight: 'bold',
+      backgroundColor: '#4285F4',
+      color: '#fff',
+  },
+  paid: {
+    textAlignVertical: 'center',
+    textAlign: 'center', 
+    paddingHorizontal: 10,
+    borderRadius:10,
+    maxHeight:30, 
+    top:12.5,
+    backgroundColor: '#2FD63C',
+    fontWeight: 'bold',
+  },
+  unpaid: {
+    textAlignVertical: 'center',
+    textAlign: 'center', 
+    paddingHorizontal: 10,
+    borderRadius:10,
+    maxHeight:30, 
+    top:12.5,
+    backgroundColor: '#E71212',
+    fontWeight: 'bold',
+  },
+});  
--- views/Home.js
+++ views/Home.js
@@ -2,14 +2,16 @@ import * as React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { BottomNavigation, Text, TextInput  } from 'react-native-paper';
 
-import History from './History';
-import Account from './Account';
+import Information from './Information';
+import CarParks from './CarParks';
 import Map from './Map';
 import FloatingActionButton from './FloatingActionButton';
+import History from './History';
+import Account from './Account';
 
-const HomeRoute = () => <Text>Information</Text>;
+const InformationRoute = () => <Information onGetList={this.Information}/>;
 
-const carParksRoute = () => <Text>Car Parks</Text>;
+const CarParksRoute = () => <CarParks onGetList={this.CarParks}/>;
 
 const MapRoute = () => 
   <View>
@@ -23,9 +25,9 @@ const AccountRoute = () => <Account onGetList={this.Account}/>;
 
 export default class HomeScreen extends React.Component {
     state = {
-      index: 0,
+      index: 2,
       routes: [
-        { key: 'home', title: 'Home', icon: 'information', color: '#7C26CB', focused:false   },
+        { key: 'info', title: 'Info', icon: 'home', color: '#7C26CB', focused:false   },
         { key: 'carParks', title: 'Car Parks', icon: 'parking', color: '#4285F4', focused:false   },
         { key: 'map', title: 'Map', icon: 'map', color: '#0F9D58', focused:true  },
         { key: 'history', title: 'History', icon: 'history', color: '#F4B400', focused:false   },
@@ -36,8 +38,8 @@ export default class HomeScreen extends React.Component {
     _handleIndexChange = index => this.setState({ index });
   
     _renderScene = BottomNavigation.SceneMap({
-        home: HomeRoute,
-        carParks: carParksRoute,
+        info: InformationRoute,
+        carParks: CarParksRoute,
         map: MapRoute,
         history: HistoryRoute,
         account: AccountRoute,
